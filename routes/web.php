5082701791fcac804ef1aad40edf8ff28400b12d
Usernamenuh <muhammadenuh4@gmail.com>
2025-06-08 23:34:51 +0700

    Latihan Laravel

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('baju', App\Http\Controllers\BajuController::class);
