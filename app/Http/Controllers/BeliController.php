2b036208f6e1982d44a804eb5a8601acaa7ac7b6
Usernamenuh <muhammadenuh4@gmail.com>
2025-06-10 22:03:56 +0700

    M

--- app/Http/Controllers/BeliController.php
+++ app/Http/Controllers/BeliController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\beli;
+use App\Models\baju;
 
 class BeliController extends Controller
 {
-    //
+    public function index()
+    {
+        $beli = beli::all();
+        $baju = baju::all();
+        return view('beli.index', compact('beli', 'baju'));
+    }
+
+    public function create() {
+        $beli = beli::all();
+        $baju = baju::all();
+        return view('beli.create', compact('beli', 'baju'));
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'jumlah' => 'required|integer',
+            'harga' => 'required|integer',
+        ], [
+            'jumlah.required' => 'Jumlah beli harus diisi',
+            'harga.required' => 'Harga beli harus diisi',
+        ]);
+
+        $beli = new beli;
+        $beli->jumlah = $request->jumlah;
+        $beli->harga = $request->harga;
+        $beli->save();
+        return redirect()->route('beli.index');
+    }
 }
