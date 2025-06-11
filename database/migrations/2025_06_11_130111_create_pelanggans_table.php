8f5be275b0c684298e4ddb0fa6b36887cf18974a
Usernamenuh <muhammadenuh4@gmail.com>
2025-06-11 20:04:34 +0700

    M

--- database/migrations/2025_06_11_130111_create_pelanggans_table.php
+++ database/migrations/2025_06_11_130111_create_pelanggans_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->string('nama');
+            $table->string('email')->unique();
             $table->integer('no_hp');
             $table->timestamps();
         });
