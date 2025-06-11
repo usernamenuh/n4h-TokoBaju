e19db61314818a32b1ff8b957c6691ae80e529f5
Usernamenuh <muhammadenuh4@gmail.com>
2025-06-11 20:10:31 +0700

    M

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\pelanggan;
 use Illuminate\Http\Request;
 
 class PelangganController extends Controller
 {
-    //
+    public function index()
+    {
+        $pelanggan = pelanggan::with('user')->latest()->get();
+        return view('pelanggan.index', compact('pelanggan'));
+    }
+
+    public function create()
+    {
+        $pelanggan = pelanggan::all();
+        return view('pelanggan.create', compact('pelanggan'));
+    }
+
+
 }
--- app/Models/pelanggan.php
+++ app/Models/pelanggan.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class pelanggan extends Model
 {
-    //
+    protected $table = 'pelanggans';
+    protected $fillable = ['user_id', 'nama', 'email', 'no_hp'];
+
+    public function user()
+    {
+        $this->hasOne(user::class);
+    }
 }
